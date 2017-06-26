fce95243f2bd7be31c0eec469e2dd5d8cbbd1484
Kristina Feller <feller.kristy@gmail.com>
2017-06-25 20:24:50 -0700

    Updated format.

--- wordsretriving.py
+++ wordsretriving.py
@@ -3,26 +3,26 @@ def load():
 
     fl = 'index.csv'
     fname = open(fl)
-    filelst = list()
+
 
     # retrieve level title
     for line in fname:
+        filelst = list()
         words = line.split(';')
         filelst.extend(words)
-    print(filelst[1])
 
-    dictionlst = list()
+        dictionlst = list()
 
-    wordlst = filelst[0]
-    allwords = open(wordlst)
+        wordlst = filelst[0]
+        allwords = open(wordlst)
 
-    # retrieve french and english words pair
-    for word in allwords:
-        word = word.rstrip()
-        word = word.split(';')
-        wordcouple = ((word[0], word[1]))
-        dictionlst.append(wordcouple)
-    print(dictionlst)
-    result.append((filelst[1], dictionlst))
+        # retrieve french and english words pair
+        for word in allwords:
+            word = word.rstrip()
+            word = word.split(';')
+            wordcouple = ((word[0], word[1]))
+            dictionlst.append(wordcouple)
+        print(dictionlst)
+        result.append((filelst[1], dictionlst))
 
     return result

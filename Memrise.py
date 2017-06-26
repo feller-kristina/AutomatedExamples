6c5c928619a5e0ecb7cb83ad857331e25c7d9fca
Kristina Feller <feller.kristy@gmail.com>
2017-06-25 20:00:45 -0700

    Updated format.

--- Memrise.py
+++ Memrise.py
@@ -1,7 +1,8 @@
 from time import sleep
 from selenium import webdriver
 
-def Login(driver):
+
+def login(driver):
     url = "https://www.memrise.com/login/"
     driver.get(url)
 
@@ -15,7 +16,8 @@ def Login(driver):
     button = driver.find_element_by_class_name("btn-success")
     button.click()
 
-def AddLevel(driver, level):
+
+def add_level(driver, level):
     # open url
     url = "https://www.memrise.com/course/1570754/horizons-6th-edition/edit/"
     driver.get(url)
@@ -36,31 +38,34 @@ def AddLevel(driver, level):
     levelname.find_element_by_tag_name("input").clear()
     levelname.send_keys(level)
 
-def AddWords(driver, fr, eng):
+
+def add_phrase(driver, fr, eng):
     # add French words to 1st field
-    word = driver.find_element_by_xpath(".//div[@class='level']//td[@data-key='1']//input[@type='text']")
-    word.send_keys(fr)
+    phrase_fr = driver.find_element_by_xpath(".//div[@class='level']//td[@data-key='1']//input[@type='text']")
+    phrase_fr.send_keys(fr)
 
     # add English words to 2nd field
-    word1 = driver.find_element_by_xpath(".//div[@class='level']//td[@data-key='2']//input[@type='text']")
-    word1.send_keys(eng)
+    phrase_eng = driver.find_element_by_xpath(".//div[@class='level']//td[@data-key='2']//input[@type='text']")
+    phrase_eng.send_keys(eng)
 
     # click add button
     add = driver.find_element_by_xpath(".//div[@class='level']//i[@class='ico ico-plus ico-blue']")
     add.click()
     sleep(1)
 
-def Save(driver):
+
+def save(driver):
     savebutton = driver.find_element_by_link_text("Save and continue")
     savebutton.click()
 
+
 def post(level, words):
     driver = webdriver.Firefox()
-    Login(driver)
-    AddLevel(driver, level)
+    login(driver)
+    add_level(driver, level)
 
     for fr, eng in words:
-        AddWords(driver, fr, eng)
+        add_phrase(driver, fr, eng)
 
-    Save(driver)
+    save(driver)
     # driver.quit()
